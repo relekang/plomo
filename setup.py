fc24d422058a806dec6d140a9dcb9f821d615b4d
Rolf Erik Lekang <me@rolflekang.com>
2014-01-24 23:15:47 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="plomo",
+    version='0.1.0',
+    description='',
+    url='http://github.com/relekang/plomo',
+    author='Rolf Erik Lekang',
+    packages=find_packages(),
+    install_requires=[
+        'pyexif',
+    ],
+    entry_points={
+        'console_scripts': [
+            'plomo = plomo:main',
+        ]
+    }
+)
