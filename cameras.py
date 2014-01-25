ea63ed1eabc5d202b198c183af118103c52d590e
Rolf Erik Lekang <me@rolflekang.com>
2014-01-24 23:39:42 -0800

    add a camera dictionary

---
+++ cameras.py
@@ -0,0 +1,9 @@
+CAMERAS = {
+    'fisheye2': {
+        'Make': 'Lomography',
+        'Model': 'Fisheye2',
+        'FocalLength':  '10mm',
+        'ExposureTime': '1/100',
+        'ApertureValue': '8'
+    }
+}
