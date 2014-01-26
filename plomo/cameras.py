774a6b14308c2005351c91533e3c24ab82c47bc5
Rolf Erik Lekang <me@rolflekang.com>
2014-01-25 22:49:39 -0800

    workaround to be able to use spaces in Exif values, fix #7

--- plomo/cameras.py
+++ plomo/cameras.py
@@ -1,14 +1,14 @@
 CAMERAS = {
     'fisheye2': {
         'Make': 'Lomography',
-        'Model': 'Fisheye2',
+        'Model': '"Fisheye 2"',
         'FocalLength':  '10mm',
         'ExposureTime': '1/100',
         'ApertureValue': '8'
     },
     'lasardina': {
         'Make': 'Lomography',
-        'Model': 'LaSardina',
+        'Model': '"La Sardina"',
         'FocalLength':  '22mm',
         'ExposureTime': '1/100',
         'ApertureValue': '8'
