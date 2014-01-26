9f3adcf4ba2d8e1a98f84b3b1dc5580781e76e3f
Truls Hamborg <truls.hamborg@gmail.com>
2014-01-25 23:00:52 -0800

    added holga 120 camera

--- plomo/cameras.py
+++ plomo/cameras.py
@@ -12,5 +12,19 @@ CAMERAS = {
         'FocalLength':  '22mm',
         'ExposureTime': '1/100',
         'ApertureValue': '8'
+    },
+    'holga120n': {
+        'Make': 'Holga',
+        'Model': '"120N"',
+        'FocalLength':  '60mm',
+        'ExposureTime': '1/100',
+        'ApertureValue': '8'  
+    },
+    'holga120fn': {
+        'Make': 'Holga',
+        'Model': '"120FN"',
+        'FocalLength':  '60mm',
+        'ExposureTime': '1/100',
+        'ApertureValue': '8'  
     }
 }
