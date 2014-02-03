ff6668b08730f8e400c4dcc117b6017420bc0826
Rolf Erik Lekang <me@rolflekang.com>
2014-02-02 19:39:42 -0800

    fix focal length data

--- plomo/equipment.py
+++ plomo/equipment.py
@@ -2,53 +2,53 @@ CAMERAS = {
     'dianaf+': {
         'Make': 'Lomography',
         'Model': 'Diana F+',
-        'FocalLength':  '75.0 mm',
+        'FocalLength':  '75.0mm',
         'ExposureTime': '1/100',
         'ApertureValue': 8
     },
     'fisheye1': {
         'Make': 'Lomography',
         'Model': 'Fisheye One',
-        'FocalLength':  '10.0 mm',
+        'FocalLength':  '10.0mm',
         'ExposureTime': '1/100',
         'ApertureValue': 8
     },
     'fisheye2': {
         'Make': 'Lomography',
         'Model': '"Fisheye No. 2"',
-        'FocalLength':  '10.0 mm',
+        'FocalLength':  '10.0mm',
         'ExposureTime': '1/100',
         'ApertureValue': 8
     },
     'holga120n': {
         'Make': 'Holga',
         'Model': '"120N"',
-        'FocalLength':  '60.0 mm',
+        'FocalLength':  '60.0mm',
         'ExposureTime': '1/100',
         'ApertureValue': 8
     },
     'holga120fn': {
         'Make': 'Holga',
         'Model': '"120FN"',
-        'FocalLength':  '60.0 mm',
+        'FocalLength':  '60.0mm',
         'ExposureTime': '1/100',
         'ApertureValue': 8
     },
     'lasardina': {
         'Make': 'Lomography',
         'Model': '"La Sardina"',
-        'FocalLength':  '22.0 mm',
+        'FocalLength':  '22.0mm',
         'ExposureTime': '1/100',
         'ApertureValue': 8
     },
     'lca': {
         'Make': 'Lomo',
         'Model': '"LC-A"',
-        'FocalLength': '32.0 mm'
+        'FocalLength': '32.0mm'
     },
     'lcw': {
         'Make': 'Lomo',
         'Model': '"LC-W"',
-        'FocalLength': '17.0 mm'
+        'FocalLength': '17.0mm'
     }
 }
